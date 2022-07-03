babdecc32a83bf55b1b11c33d24ca135c4b8f80d
Ravil <ravilgab@yahoo.com>
2022-07-03 19:29:37 +0300

    Log in logic is completed using segues and unwind. Need to fix button visuals with code.

---
+++ Homework4_2.28/GreetingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GreetingsViewController.swift
+//  Homework4_2.28
+//
+//  Created by Ravil on 03.07.2022.
+//
+
+import UIKit
+
+class GreetingsViewController: UIViewController {
+    
+    @IBOutlet weak var greetingsLabel: UILabel!
+    
+    var user = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        greetingsLabel.text = "Welcome \(user)!"
+    }
+
+}
---
+++ Homework4_2.28/LogInViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  Homework4_2.28
+//
+//  Created by Ravil on 01.07.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    private let userName = "Tim"
+    private let password = "password"
+
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let greetingsScreenVC = segue.destination as? GreetingsViewController else { return }
+        greetingsScreenVC.user = userNameTF.text ?? ""
+    }
+
+    @IBAction func logInButtonPressed() {
+        if userNameTF.text != userName || passwordTF.text != password {
+            credentialsFailAlert()
+            passwordTF.text = ""
+        }
+    }
+    
+    @IBAction func forgotUserNamePressed() {
+        let alert = UIAlertController(title: "Oops!",
+                                      message: "Your username is Tim",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok!", style: .default))
+        present(alert, animated: true)
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        let alert = UIAlertController(title: "Oops!",
+                                      message: "Your password is password",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Thanks!", style: .cancel))
+        present(alert, animated: true)
+    }
+    
+    private func credentialsFailAlert() {
+        let alert = UIAlertController(title: "Invalid username or password",
+                                      message: "Please enter correct username and password",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true)
+    }
+    
+    @IBAction func unwindSegue(_ unwindSegue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+}
+
+extension LogInViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTF {
+            passwordTF.becomeFirstResponder()
+        } else {
+            logInButtonPressed()
+            performSegue(withIdentifier: "showGreetingsVC", sender: nil)
+        }
+        
+        return true
+    }
+}
+
